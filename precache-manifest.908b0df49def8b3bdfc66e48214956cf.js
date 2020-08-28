c8427c31518290a7ed0d8104e1522526080f530a
KPR <64140329+kpproce@users.noreply.github.com>
2020-08-28 11:50:00 +0200

    Updates

---
+++ precache-manifest.908b0df49def8b3bdfc66e48214956cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3067c6f3cae223f36af6b0403ea6439d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "9ad88e9810e8177559a4",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0695a6e83d5ececa6024",
+    "url": "/test/static/js/2.b376dd8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.b376dd8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad88e9810e8177559a4",
+    "url": "/test/static/js/main.94e67afa.chunk.js"
+  },
+  {
+    "revision": "7826bcd79f3a3435bc0c",
+    "url": "/test/static/js/runtime-main.fb3bb2cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
