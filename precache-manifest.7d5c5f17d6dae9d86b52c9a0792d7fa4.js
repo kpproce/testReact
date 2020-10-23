f82c343e9b4bd6e9d8b252135637f7aceb8e3d1e
KPR <64140329+kpproce@users.noreply.github.com>
2020-10-23 17:04:45 +0200

    Updates

---
+++ precache-manifest.7d5c5f17d6dae9d86b52c9a0792d7fa4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d956467fbe0529464184a6eee45469f",
+    "url": "/testreact/index.html"
+  },
+  {
+    "revision": "8813efce8a89e2c113e2",
+    "url": "/testreact/static/css/2.a9f666f8.chunk.css"
+  },
+  {
+    "revision": "307b46bc2c59bbe168c3",
+    "url": "/testreact/static/css/main.7ebba2f3.chunk.css"
+  },
+  {
+    "revision": "8813efce8a89e2c113e2",
+    "url": "/testreact/static/js/2.3e4d0895.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/testreact/static/js/2.3e4d0895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307b46bc2c59bbe168c3",
+    "url": "/testreact/static/js/main.8dcddb4c.chunk.js"
+  },
+  {
+    "revision": "f572aaebef4f0f82f4ba3245e3a3e73a",
+    "url": "/testreact/static/js/pdf.worker.entry.1676b5d0.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "/testreact/static/js/pdf.worker.entry.1676b5d0.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cccab1befa68d1ad864",
+    "url": "/testreact/static/js/runtime-main.b1e4199e.js"
+  }
+]);
