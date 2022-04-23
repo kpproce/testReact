33e0473f5d4f75ed977a5765e27f413040a5f4f1
KPR <64140329+kpproce@users.noreply.github.com>
2022-04-23 19:44:53 +0200

    Updates

---
+++ precache-manifest.cea61ee599955c18cd269145cf1ebe08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7e90e5901d455f68148",
+    "url": "/testreact/static/css/main.a7bfd4ef.chunk.css"
+  },
+  {
+    "revision": "b7e90e5901d455f68148",
+    "url": "/testreact/static/js/main.4868740c.chunk.js"
+  },
+  {
+    "revision": "1d0bc4833f5a10c60360",
+    "url": "/testreact/static/js/runtime~main.30e0e851.js"
+  },
+  {
+    "revision": "0baa56767229dd80850b",
+    "url": "/testreact/static/css/2.a7284484.chunk.css"
+  },
+  {
+    "revision": "0baa56767229dd80850b",
+    "url": "/testreact/static/js/2.2a96e428.chunk.js"
+  },
+  {
+    "revision": "469dffc023486545b273169c8a640608",
+    "url": "/testreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/testreact/precache-manifest.a570a24fbdb917415c933cb078be0441.js"
+  "/testreact/precache-manifest.cea61ee599955c18cd269145cf1ebe08.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testreact/index.html"), {
+workbox.routing.registerNavigationRoute("/testreact/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
