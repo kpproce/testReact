987a46c5da09d3c106286cbfbc98740b8a7c451f
KPR <64140329+kpproce@users.noreply.github.com>
2022-06-05 08:53:26 +0200

    5 juni V1

--- src/components/PlayListV4.js
+++ src/components/PlayListV4.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from 'react';
 // import { Link } from 'react-router-dom'
 import {Button} from 'react-bootstrap';
 import SongDetailsModal from './SongDetailsModal';
+import SongInsert from './SongINSERT';
 import PDFViewerV1 from './PDFViewerV1';
+
 // import UploadFile from './UploadFile';
 
 //  ModSecurity stond niet aan bij hostingpartij bhosted: opgelost 
@@ -19,36 +21,16 @@ const PlayList = (props) => {
   const [data, setData] = useState(null)
   const [showAPIResNoData, setshowAPIResNoData] = useState(false) // for debug and error handling
   const [filter, setFilter] = useState("") // 
-  // const [code, setCode] = useState("10") // code voor api, die geeft geen data bij foute code.
   const [debug, setDebug] = useState(false) // 
+  const [sortOnSongNameChecked, setSortOnSongNameChecked] = useState(false) // 
+  
+  
   const [groupName, setGroupName] = useState(props.groupName) 
   const [parentName, setParentName] = useState(props.parentName) 
 
-  const [code, setCode] = useState(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem('code');
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : '10';
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error);
-      return '10';
-    }
-  });
-  
-  const [username, setUsername] = useState(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem('username');
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : 'guest';
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error);
-      return 'guest';
-    }
-  });
+  const [code, setCode] = useState(props.code); 
+  const [username, setUsername] =  useState(props.username);
+  const [newItemToegevoegd, setNewItemToegevoegd] = =  useState(false);
 
   // check local or provider 
 
@@ -61,26 +43,52 @@ const PlayList = (props) => {
   
   // alert(hostName)
 
-  const changeCode = (c) => {
-    setCode(c);
-    setChildChanged(true);
-  }
-
   const requestOptions = {
     method: 'POST',
     headers: {
       'Accept': 'application/json, text/plain, */*',
       'Content-Type': 'application/json'
     },
-    body:  JSON.stringify({'username': username, 'code': code, 'filter':filter, 'parentName': parentName})
+    body:  JSON.stringify({'username': username, 'code': code, 'filter':filter, 'parentName': parentName, 'folder':'songs' } )
   };
 
-    const getData = () =>
-    fetch(fetchURL, requestOptions)
-      .then((res) => {
-        if (!res.ok) { throw res }
-        return res.json()
-      })
+  const getData = () =>
+  fetch(fetchURL, requestOptions)
+    .then((res) => {
+      if (!res.ok) { throw res }
+      return res.json()
+    })
+
+  function sortData() {
+    // sort by name
+    if (data) {
+      let valueA = ""
+      let valueB = ""
+      data.resData.sort(function(a, b) {
+        if (!sortOnSongNameChecked) {
+          valueA = a.title.toUpperCase(); // ignore upper and lowercase
+          valueB = b.title.toUpperCase(); // ignore upper and lowercase
+          console.log("Sort data --> checked")
+        } else {
+          valueA = a.groupName.toUpperCase(); // ignore upper and lowercase
+          valueB = b.groupName.toUpperCase(); // ignore upper and lowercase
+          console.log("Sort data --> UN-checked")
+        }
+        if (valueA < valueB) {
+          return -1;
+        }
+        if (valueA > valueB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+    }
+  }
+
+  useEffect(() => {
+  
+  },[sortOnSongNameChecked])
 
   function callBack() {    
     setChildChanged(true);
@@ -91,9 +99,17 @@ const PlayList = (props) => {
   useEffect(() => {
     //alert(window.location.hostname)
     getData()
-    .then((data) => setData(data))
-    .then (setChildChanged(false))
-    // .then(alert ('data received'))
+    .then((data) => {
+      setData(data)
+      console.log('resdata in use effect')
+      console.log(data.resData)
+
+
+      setChildChanged(false)
+    }, [newItemToegevoegd])
+    
+  
+    // .then(alert (data.resData[0].title))
   }, [childChanged])
 
   return (
@@ -101,27 +117,34 @@ const PlayList = (props) => {
     <>
       <table border="1" className="messageContainer">
         <thead>
-          <tr> <th className = 'SLTH'> </th> </tr> 
+          <tr><th className = 'SLTH'></th></tr>
         </thead>
-
         <tbody>
-          <tr><td  colSpan="2">
-            Fllter:{" "}
+          <tr><td colSpan="2">
+        {/*     Filter aan/uit:{" "}
             <input
               name="isGoing"
               type="checkbox"
               checked={debug}
               onChange={e => {setDebug(e.target.checked) }}
             />
+            {'  '} */}
+            Sorteer:{" "}
+            <input
+              name="sortOnSongName"
+              type="checkbox"
+              checked={sortOnSongNameChecked}
+              onChange={e => {
+                setSortOnSongNameChecked(e.target.checked) 
+                console.log('resdata in return voor sortdata')
+                sortData() 
+                console.log('resdata in return na sortdata')
+               }}
+            />
 
-            {(data["edit"])?
+            {(data["edit"] || data["admin"])?
               <>            
-              {//childChanged? 'childChanged = true' :"childChanged = false"
-              }
-              <button 
-                className= "small width30 " 
-                onClick ={ () => setshowAPIResNoData(!showAPIResNoData)}>{showAPIResNoData? 'hide APIinfo':'APIinfo'}
-              </button>
+               {' '}<SongInsert username={username} code={code} groupName={props.groupName} parentName={props.parentName} bron={username} showNewButton={true}  callBack={callBack} />
             </>
             :""
             }
@@ -129,40 +152,16 @@ const PlayList = (props) => {
           {debug?
             <> 
               <tr><td  colSpan="2">
-                {"Filter"}<input className= "small width30" name="name" value={filter} onChange={e => setFilter(e.target.value)}/>
+                {"Filter 1"}<input className= "small width30" name="name" value={filter} onChange={e => setFilter(e.target.value)}/>
                 {" "}<button className= "small width30 " onClick ={ () => setChildChanged(true)}> refresh page</button>{" "} 
               </td></tr>
             </>
             :""
           }
-          {//  id	  order1	  title	  artist	  afspraken	  videoURL1	  musicSheetURL1	  musicSheetURL2	  image1	  image2
-          }
           {
             data["done"] !== undefined? 
               <>
-              {showAPIResNoData? // toon response van de APi hier eventueel.
-                  <>
-                    {// zou ook met een loop kunnen maar info is per item anders en dus niet handig
-                    }
-                    <tr><td>accessCodeOkay (authorisatie?){": "}
-                      {data["accessCodeOkay"] !== undefined? data["accessCodeOkay"]?"Okay":"Code niet okay":"Niet door API verstuurd"}
-                    </td></tr>
-                    <tr><td>done (verzoek door Server uitgevoerd?){": "}
-                      {data["done"] !== undefined? data["done"]?"Ja":"Nee, is wat fout gegaan":"Niet door API verstuurd"}
-                    </td></tr>
-                    <tr><td>message (info van server){": "}
-                      {data["message"] !== undefined? JSON.stringify(data["message"]):"Niet door API verstuurd"}
-                    </td></tr>
-                    <tr><td> errorMessage? (bij verwerken server){": "}
-                      {data["errorMessage"] !== undefined? JSON.stringify(data["errorMessage"]):"Niet door API verstuurd"}
-                    </td></tr>
-                    <tr><td>rowCount (aantal aangepast/getoond){": "}
-                      {data["rowCount"] !== undefined? JSON.stringify(data["rowCount"]):"Niet door API verstuurd"}
-                    </td></tr>
-                  </>
-                : <tr><td></td></tr>
-                }
-                   {data["rowCount"]<1?"Er zijn geen songs. Mogelijk onvoldoende rechten. Login met guest/10, of demo/demo":""}    
+                  {data["rowCount"]<1?"Er zijn geen songs. Mogelijk onvoldoende rechten. Login met guest/10, of demo/demo":""}    
                   {data["resData"] !== undefined?            
                     <>
                       {
@@ -170,15 +169,23 @@ const PlayList = (props) => {
                  
                       data["resData"].map((item,index) =>             
                       <tr key={index}>
-                        <td> <span className='xxSmall'>{item['groupName']}</span><br/>
+                        <td> 
+                                                  
+              
                           {item['title']} {item['artist']} <span className="small"> {item['afspraken']} </span> <br/>
+
+                          <img src={process.env.PUBLIC_URL + '/' + item['groupName'] + '.jpg'} alt = {item['groupName']} width="35"/>{" "} 
+
+
+
                           {( data["edit"] || data["demo"])?<SongDetailsModal callBack={callBack} song={item} groupNameList={props.groupNameList}/>:"" }{" "} 
                           {item['videoURL1']?
                           <a href={ item['videoURL1']} target="_blank" rel="noopener noreferrer" download>
-                            <Button> <i className="fas fa-download"/>  Video </Button> {" "} 
+                            <Button> <i className="fas fa-download"/> <span className="xSmall"> Video </span>  </Button> {" "} 
                           </a>
                         :''
                         }
+
 
                         {item['videoURL2']?
                           <a href={ item['videoURL2']} target="_blank" rel="noopener noreferrer" download>
@@ -189,14 +196,14 @@ const PlayList = (props) => {
 
                         {item['musicSheetURL1']?
                           <a href={ item['musicSheetURL1']} target="_blank" rel="noopener noreferrer" download>
-                            <Button> <i className="fas fa-download"/> Chords </Button> {" "} 
+                            <Button> <i className="fas fa-download"/> <span className="xSmall"> Chords </span> </Button> {" "} 
                           </a>
                         :''
                         }
 
                         {item['musicSheetURL2']?
                           <a href={ item['musicSheetURL2']} target="_blank" rel="noopener noreferrer" download>
-                            <Button><i className="fas fa-download"/> Chords(2) </Button> {" "} 
+                            <Button><i className="fas fa-download"/> <span className="xSmall"> Chords (2)</span>  </Button> {" "} 
                           </a>
                         :''
                         }
