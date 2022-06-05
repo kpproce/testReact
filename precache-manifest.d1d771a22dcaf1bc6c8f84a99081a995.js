fc18da2f452275fe5de1a4852cba5b9d2813136a
KPR <64140329+kpproce@users.noreply.github.com>
2022-06-05 09:03:10 +0200

    Updates

---
+++ precache-manifest.d1d771a22dcaf1bc6c8f84a99081a995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ef7f3df1b9e7f77659",
+    "url": "/testreact/static/css/main.4be50e96.chunk.css"
+  },
+  {
+    "revision": "02ef7f3df1b9e7f77659",
+    "url": "/testreact/static/js/main.73c66ebf.chunk.js"
+  },
+  {
+    "revision": "1d0bc4833f5a10c60360",
+    "url": "/testreact/static/js/runtime~main.30e0e851.js"
+  },
+  {
+    "revision": "aa846ec6da104b1a60a4",
+    "url": "/testreact/static/css/2.a7284484.chunk.css"
+  },
+  {
+    "revision": "aa846ec6da104b1a60a4",
+    "url": "/testreact/static/js/2.d9ab269d.chunk.js"
+  },
+  {
+    "revision": "fbdd80b354aadbc4fa71a58c08ab0b4e",
+    "url": "/testreact/index.html"
+  }
+];
